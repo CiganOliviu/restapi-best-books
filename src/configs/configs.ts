cdfc5fd6956ccd1f5c0e5a06e080449378d2258a
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2022-02-21 22:52:22 +0200

    ERASE ALL

--- src/configs/configs.ts
+++
@@ -1,5 +0,0 @@
-export default {
-    port: 8088 as number,
-    host: 'localhost' as string,
-    dbUrl: "mongodb://localhost:27017/books", 
-}
--- src/controllers/getBooksEntries.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-import booksModel from '../models/books.model';
-
-export async function getBooksEntries(request: Request, response: Response) {
-    
-    const data = await booksModel.find({});
-
-    return response.send(data[0].title);
-}
