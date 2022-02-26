7bd6c4821a9e0be0587fab8e1e1c676dd79c3a9d
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2022-02-26 22:00:19 +0200

    Add route for categories

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,11 @@
 import { Express } from 'express'; 
 import { operateOverAuthorsController } from '../controllers/authors.controller';
 import { operateOverBooksController } from '../controllers/books.controller';
+import { operateOverCategoriesController } from '../controllers/categories.controller';
 
 export function setRoutes(app: Express) {
 
+    app.use('/categories', operateOverCategoriesController);
     app.use('/authors', operateOverAuthorsController);
     app.use('/books', operateOverBooksController);
 }
