43675b3fecadd68b4299e14dbc3f5185f562de3b
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2022-02-26 21:23:04 +0200

    Add route for operateOverBooksController

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,7 +1,9 @@
 import { Express } from 'express'; 
 import { operateOverAuthorsController } from '../controllers/authors.controller';
+import { operateOverBooksController } from '../controllers/books.controller';
 
 export function setRoutes(app: Express) {
 
     app.use('/authors', operateOverAuthorsController);
+    app.use('/books', operateOverBooksController);
 }
